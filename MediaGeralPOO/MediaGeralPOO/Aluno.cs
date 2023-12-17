ac9ef80ae1228e98ffdd84d67dcb546cdfa87d84
Renan Moreira <105755795+moreirarmcm@users.noreply.github.com>
2023-12-17 14:35:37 -0300

    Primeiro projeto em C#

---
+++ MediaGeralPOO/MediaGeralPOO/Aluno.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaGeralPOO
+{
+    internal class Aluno
+    {
+        public String nome { get; private set; }
+        public double[] notas;
+
+        public Aluno(string nome, int provas)
+        {
+            this.nome = nome;
+            notas = new double[provas];
+        }
+        public void InformarNotas()
+        {
+            for (int i = 0; i < notas.Length; i++)
+            {
+                Console.Write("Digite o valor da nota " + (i + 1) + ": ");
+                try
+                {
+                    this.notas[i] = Double.Parse(Console.ReadLine());
+                    //Console.Write(notas[i]);
+                }catch (Exception ex) {
+                    Console.WriteLine("Digite uma nota válida!");
+                    i--;
+                }
+            }
+        }
+
+        public double RetornarMedia() {
+            double media = 0;
+            for (int i = 0; i < notas.Length; i++)  {
+                    media = media + notas[i];
+                }
+        
+            return media/notas.Length;
+        }
+
+    }
+}
---
+++ MediaGeralPOO/MediaGeralPOO/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaGeralPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Média Geral dos Alunos";
+            Console.Write("Quantos alunos deseja cadastrar?\n");
+            int qtdade_alunos = 0;
+            String nome_aluno;
+            int notas_aluno;
+            Aluno[] alunos;
+
+            while (qtdade_alunos == 0)
+            {
+                try
+                {
+                    qtdade_alunos = int.Parse(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Digite um número inteiro");
+                }
+            }
+            alunos = new Aluno[qtdade_alunos];
+            for (int i = 0;i<alunos.Length;i++){
+                    Console.Write("Qual o nome do aluno #" + (i+1) + "?\n");
+                    try
+                    {
+                        nome_aluno = (Console.ReadLine());
+                        Console.Write("Quantas notas o aluno " + nome_aluno + " possui?\n");
+                        notas_aluno = int.Parse(Console.ReadLine());
+                    alunos[i] = new Aluno(nome_aluno, notas_aluno);
+                    alunos[i].InformarNotas();
+                    Console.WriteLine ("A média do '" + nome_aluno + "' é: " + alunos[i].RetornarMedia());
+                }
+                    catch(Exception ex) {
+                        Console.WriteLine("Nome ou notas inválidos. Revise os dados informados.");
+                        i--;
+                    }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
