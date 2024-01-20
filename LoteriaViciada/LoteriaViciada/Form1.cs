927816e35023e5510f9c44266a307384dd023c0b
Renan Moreira <105755795+moreirarmcm@users.noreply.github.com>
2024-01-19 23:21:12 -0300

    .

--- LoteriaViciada/LoteriaViciada/Form1.cs
+++ LoteriaViciada/LoteriaViciada/Form1.cs
@@ -136,7 +136,7 @@ namespace LoteriaViciada
             else
             {
                 jogador = new Jogador(txtNome.Text, escolha);
-                //
+                
             }
         }
     }
