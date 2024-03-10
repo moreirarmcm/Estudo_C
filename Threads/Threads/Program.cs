8095b14e1a923f6c54c372e402a66bb7cf195bc6
Renan Moreira <105755795+moreirarmcm@users.noreply.github.com>
2024-03-10 10:45:10 -0300

    .
    .

---
+++ Threads/Threads/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(Tarefa)); 
+            t.Start();
+
+            for (int i = 0; i < 10; i++) {
+                Console.WriteLine("Principal");
+                Dormir();
+
+            }
+            Console.ReadKey();
+           
+        }
+
+        static void Tarefa()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Thread");
+                Dormir();
+            }
+        }
+
+        static void Dormir()
+        {
+            Thread.Sleep(1000);
+        }
+
+
+    }
+}
