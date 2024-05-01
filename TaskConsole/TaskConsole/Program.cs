469e1c10b89e1c91563c1a8137890a91c575084f
Renan Moreira <105755795+moreirarmcm@users.noreply.github.com>
2024-05-01 13:42:56 -0300

    .

---
+++ TaskConsole/TaskConsole/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task Parte 1
+
+            // Task t1 = new Task(Tarefa);
+            //t1.Start();
+
+            //  Task t2 = Task.Run(Tarefa);
+
+            //  Task.Run(Tarefa);
+
+            //Task.Factory.StartNew(Tarefa);
+
+            //Task.Run(() =>
+            //{
+            //    for (int i = 0; i < 10; i++)
+            //    {
+            //        Console.WriteLine("Tarefa anônima " + i);
+            //    }
+            //});
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("Principal.");
+            //}
+            //Console.ReadLine();
+            #endregion
+
+            #region Task Parte 2
+            //Task[] tasks = 
+            //    {
+            //        Task.Factory.StartNew(() => {Console.WriteLine("Tarefa #1"); }),
+            //        Task.Factory.StartNew(() => {Console.WriteLine("Tarefa #2");}),
+            //        Task.Factory.StartNew(() => {Console.WriteLine("Tarefa #3");}),
+            //        Task.Factory.StartNew(() => {Console.WriteLine("Tarefa #4");}),
+            //        Task.Factory.StartNew(() => {Console.WriteLine("Tarefa #5");})
+            //    };
+            //Task.WaitAll(tasks);
+            //Console.WriteLine("Principal");
+            //Console.ReadLine();
+            #endregion
+
+            Task<int> tarefa1 = Task.Factory.StartNew(() => 
+            {
+                return new Random().Next(10);
+            });
+
+            Task<int> tarefa2 = tarefa1.ContinueWith((num) => { return Dobro(num.Result); });//; Dobro(tarefa1.Result) });
+            Task<String> tarefa3 = tarefa2.ContinueWith((num) => {
+                    return "Valor final é " + num.Result;
+                });//; Dobro(tarefa1.Result) });
+
+
+            Console.WriteLine(tarefa3.Result);
+            Console.ReadKey();
+        }
+
+        static int Dobro(int x)
+        {
+            return x * 2;
+        }
+
+        static private void Tarefa()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Tarefa " + i);
+            }
+        }
+
+    }
+}
---
+++ ThreadsEstudo/ThreadsEstudo/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ThreadsEstudo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrincipal = new System.Windows.Forms.Button();
+            this.btnContador = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnPrincipal
+            // 
+            this.btnPrincipal.Location = new System.Drawing.Point(142, 300);
+            this.btnPrincipal.Name = "btnPrincipal";
+            this.btnPrincipal.Size = new System.Drawing.Size(160, 48);
+            this.btnPrincipal.TabIndex = 0;
+            this.btnPrincipal.Text = "Principal";
+            this.btnPrincipal.UseVisualStyleBackColor = true;
+            this.btnPrincipal.Click += new System.EventHandler(this.btnPrincipal_Click);
+            // 
+            // btnContador
+            // 
+            this.btnContador.Location = new System.Drawing.Point(344, 300);
+            this.btnContador.Name = "btnContador";
+            this.btnContador.Size = new System.Drawing.Size(160, 48);
+            this.btnContador.TabIndex = 1;
+            this.btnContador.Text = "Contador";
+            this.btnContador.UseVisualStyleBackColor = true;
+            this.btnContador.Click += new System.EventHandler(this.btnContador_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.Location = new System.Drawing.Point(12, 9);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(634, 131);
+            this.lblResultado.TabIndex = 2;
+            this.lblResultado.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 450);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnContador);
+            this.Controls.Add(this.btnPrincipal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrincipal;
+        private System.Windows.Forms.Button btnContador;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
