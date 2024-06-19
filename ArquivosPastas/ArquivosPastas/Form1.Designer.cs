a6c755148509a6bdc4c0ae8c61efbb360c6a096b
Renan Moreira <105755795+moreirarmcm@users.noreply.github.com>
2024-06-18 22:37:40 -0300

    .

--- ArquivosPastas/ArquivosPastas/Form1.Designer.cs
+++ ArquivosPastas/ArquivosPastas/Form1.Designer.cs
@@ -30,11 +30,14 @@
         {
             this.lblResultado = new System.Windows.Forms.Label();
             this.btnExecutar = new System.Windows.Forms.Button();
+            this.btnCopiar = new System.Windows.Forms.Button();
+            this.btnMover = new System.Windows.Forms.Button();
+            this.btnDeletar = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblResultado
             // 
-            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblResultado.Location = new System.Drawing.Point(44, 78);
             this.lblResultado.Name = "lblResultado";
             this.lblResultado.Size = new System.Drawing.Size(716, 68);
@@ -44,20 +47,56 @@
             // 
             // btnExecutar
             // 
-            this.btnExecutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExecutar.Location = new System.Drawing.Point(23, 298);
+            this.btnExecutar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExecutar.Location = new System.Drawing.Point(12, 204);
             this.btnExecutar.Name = "btnExecutar";
             this.btnExecutar.Size = new System.Drawing.Size(154, 55);
             this.btnExecutar.TabIndex = 1;
-            this.btnExecutar.Text = "Executar";
+            this.btnExecutar.Text = "Criar documento";
             this.btnExecutar.UseVisualStyleBackColor = true;
             this.btnExecutar.Click += new System.EventHandler(this.btnExecutar_Click);
+            // 
+            // btnCopiar
+            // 
+            this.btnCopiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCopiar.Location = new System.Drawing.Point(12, 265);
+            this.btnCopiar.Name = "btnCopiar";
+            this.btnCopiar.Size = new System.Drawing.Size(154, 55);
+            this.btnCopiar.TabIndex = 2;
+            this.btnCopiar.Text = "Copiar documento";
+            this.btnCopiar.UseVisualStyleBackColor = true;
+            this.btnCopiar.Click += new System.EventHandler(this.btnCopiar_Click);
+            // 
+            // btnMover
+            // 
+            this.btnMover.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMover.Location = new System.Drawing.Point(12, 326);
+            this.btnMover.Name = "btnMover";
+            this.btnMover.Size = new System.Drawing.Size(154, 55);
+            this.btnMover.TabIndex = 3;
+            this.btnMover.Text = "Mover documento";
+            this.btnMover.UseVisualStyleBackColor = true;
+            this.btnMover.Click += new System.EventHandler(this.btnMover_Click);
+            // 
+            // btnDeletar
+            // 
+            this.btnDeletar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDeletar.Location = new System.Drawing.Point(12, 387);
+            this.btnDeletar.Name = "btnDeletar";
+            this.btnDeletar.Size = new System.Drawing.Size(154, 55);
+            this.btnDeletar.TabIndex = 4;
+            this.btnDeletar.Text = "Deletar documento";
+            this.btnDeletar.UseVisualStyleBackColor = true;
+            this.btnDeletar.Click += new System.EventHandler(this.btnDeletar_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 526);
+            this.Controls.Add(this.btnDeletar);
+            this.Controls.Add(this.btnMover);
+            this.Controls.Add(this.btnCopiar);
             this.Controls.Add(this.btnExecutar);
             this.Controls.Add(this.lblResultado);
             this.Name = "Form1";
@@ -70,6 +109,9 @@
 
         private System.Windows.Forms.Label lblResultado;
         private System.Windows.Forms.Button btnExecutar;
+        private System.Windows.Forms.Button btnCopiar;
+        private System.Windows.Forms.Button btnMover;
+        private System.Windows.Forms.Button btnDeletar;
     }
 }
 
--- ArquivosPastas/ArquivosPastas/Form1.cs
+++ ArquivosPastas/ArquivosPastas/Form1.cs
@@ -13,23 +13,32 @@ namespace ArquivosPastas
 {
     public partial class Form1 : Form
     {
- 
+        String pasta = @"C:\Users\morei\Documents\GitHub\Estudo_C\ArquivosPastas\Arquivos\";
+        String pasta_backup = @"C:\Users\morei\Documents\GitHub\Estudo_C\ArquivosPastas\Arquivos\Backup\";
+        String pasta_lixeira = @"C:\Users\morei\Documents\GitHub\Estudo_C\ArquivosPastas\Arquivos\Lixeira\";
+        String pasta_importante = @"C:\Users\morei\Documents\GitHub\Estudo_C\ArquivosPastas\Arquivos\Arquivos importantes\";
+
+        //"C:\\Users\\morei\\Documents\\GitHub\\Estudo_C\\ArquivosPastas\\Arquivos\\";
+        String arquivo = "Teste.txt";
+        bool has;// = File.Exists(pasta + arquivo);
+
         public Form1()
         {
             InitializeComponent();
+
         }
 
         private void btnExecutar_Click(object sender, EventArgs e)
         {
-            String pasta = @"C:\Users\morei\Documents\GitHub\Estudo_C\ArquivosPastas\Arquivos\";
+            this.pasta = @"C:\Users\morei\Documents\GitHub\Estudo_C\ArquivosPastas\Arquivos\";
             //"C:\\Users\\morei\\Documents\\GitHub\\Estudo_C\\ArquivosPastas\\Arquivos\\";
-            String arquivo = "Teste.txt";
-            bool has = File.Exists(pasta + arquivo);
+            this.arquivo = "Teste.txt";
+            this.has = File.Exists(pasta + arquivo);
             if (has)
             {
-                lblResultado.Text = "Existe";
+                lblResultado.Text = "Arquivo já existe.";
                 //Console.ReadLine();
-                File.Delete(pasta + arquivo);
+                //File.Delete(pasta + arquivo);
             }
             else
             {
@@ -38,7 +47,53 @@ namespace ArquivosPastas
             }
 
 
-           
+
+        }
+
+        private void btnCopiar_Click(object sender, EventArgs e)
+        {
+            this.has = File.Exists(pasta + arquivo);
+            if (has)
+            {
+                File.Copy(pasta + arquivo, pasta + arquivo + " (Cópia).txt", true);
+                lblResultado.Text = "Arquivo copiado!";
+            }
+            else
+            {
+                lblResultado.Text = "Arquivo fonte não existe.";
+
+            }
+        }
+
+        private void btnMover_Click(object sender, EventArgs e)
+        {
+            this.has = File.Exists(pasta + arquivo);
+            if (has)
+            {
+                if (File.Exists(pasta_importante + arquivo))
+                {
+                    File.Delete(pasta_importante + arquivo);
+                }
+                File.Move(pasta + arquivo, pasta_importante + arquivo);
+            }
+            else
+            {
+                lblResultado.Text = "O Arquivo não existe para ser movido.";
+            }
+        }
+
+        private void btnDeletar_Click(object sender, EventArgs e)
+        {
+            this.has = File.Exists(pasta + arquivo);
+            if (has)
+            {
+                File.Copy(pasta + arquivo, pasta_backup + "bkp_" + arquivo, true);
+                File.Move(pasta + arquivo, pasta_lixeira + arquivo);
+            }
+            else
+            {
+                lblResultado.Text = "Não foi possível apagar o arquivo.";
+            }
         }
     }
 }
