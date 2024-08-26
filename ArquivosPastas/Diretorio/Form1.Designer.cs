ba79db2b1b0813cc76db00da88b5e288122383f5
Renan Moreira <105755795+moreirarmcm@users.noreply.github.com>
2024-08-26 08:58:56 -0300

    .
    .

---
+++ ArquivosPastas/Diretorio/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Diretorio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Lista = new System.Windows.Forms.ListBox();
+            this.btnExiste = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Lista
+            // 
+            this.Lista.FormattingEnabled = true;
+            this.Lista.ItemHeight = 16;
+            this.Lista.Location = new System.Drawing.Point(34, 34);
+            this.Lista.Name = "Lista";
+            this.Lista.Size = new System.Drawing.Size(733, 148);
+            this.Lista.TabIndex = 0;
+            // 
+            // btnExiste
+            // 
+            this.btnExiste.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExiste.Location = new System.Drawing.Point(34, 242);
+            this.btnExiste.Name = "btnExiste";
+            this.btnExiste.Size = new System.Drawing.Size(121, 43);
+            this.btnExiste.TabIndex = 1;
+            this.btnExiste.Text = "Verificar";
+            this.btnExiste.UseVisualStyleBackColor = true;
+            this.btnExiste.Click += new System.EventHandler(this.btnExiste_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluir.Location = new System.Drawing.Point(183, 242);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(121, 43);
+            this.btnExcluir.TabIndex = 2;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnExiste);
+            this.Controls.Add(this.Lista);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox Lista;
+        private System.Windows.Forms.Button btnExiste;
+        private System.Windows.Forms.Button btnExcluir;
+    }
+}
+
---
+++ ArquivosPastas/Diretorio/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Diretorio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExiste_Click(object sender, EventArgs e)
+        {
+            string caminho = @"C:\Users\morei\Documents\GitHub\Estudo_C\ArquivosPastas\";
+            bool res = Directory.Exists(caminho + @"\Arquivos");
+            Lista.Items.Add(res);
+            if (!res)
+            {
+                Directory.CreateDirectory(caminho + "Arquivos");
+            }
+            else
+            {
+                Directory.Delete(caminho + "Arquivos");
+            }
+
+        }
+    }
+}
