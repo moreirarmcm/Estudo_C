f77f1eebb544ac49def95a83754f8208a6c7b195
Renan Moreira <105755795+moreirarmcm@users.noreply.github.com>
2024-10-02 20:55:07 -0300

    Iniciando Projeto
    Criando uma base de dados para um dojo de karate.

--- BaseKarate/1 - Criando Base e Tabelas.sql
+++ BaseKarate/1 - Criando Base e Tabelas.sql
@@ -1,23 +1,32 @@
-CREATE DATABASE BaseKarate
-
+IF NOT EXISTS (select* from sys.databases where name = 'BaseKarate')
+BEGIN
+	CREATE DATABASE BaseKarate
+END;
+	
 USE BaseKarate
 
-CREATE TABLE Aluno(
-	Codigo INT PRIMARY KEY IDENTITY,
-	Nome VARCHAR(50) NOT NULL,
-	Graduacao VARCHAR (12) NOT NULL,
-	Sexo CHAR(1),
-	Nascimento DATE,
-	CodigoResponsavel INT,
-	Criacao DATE,
-	Endereco VARCHAR(255),
-	Telefone VARCHAR(15)
-)
+IF NOT EXISTS (select* from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'Aluno')
+BEGIN
+	CREATE TABLE Aluno(
+		Codigo INT PRIMARY KEY IDENTITY,
+		Nome VARCHAR(50) NOT NULL,
+		Graduacao VARCHAR (12) NOT NULL,
+		Sexo CHAR(1),
+		Nascimento DATE,
+		CodigoResponsavel INT,
+		Criacao DATE,
+		Endereco VARCHAR(255),
+		Telefone VARCHAR(15)
+	)
+END;
 
-CREATE TABLE Responsavel (
-	Codigo INT PRIMARY KEY IDENTITY,
-	Nome VARCHAR(50) NOT NULL,
-	Endereco VARCHAR(50) NOT NULL,
-	Telefone VARCHAR(50) NOT NULL,
-	Parentesco VARCHAR(50) NOT NULL
-)	
+IF NOT EXISTS (select* from INFORMATION_SCHEMA.TABLES where TABLE_NAME = 'Responsavel')
+BEGIN
+	CREATE TABLE Responsavel (
+		Codigo INT PRIMARY KEY IDENTITY,
+		Nome VARCHAR(50) NOT NULL,
+		Endereco VARCHAR(50) NOT NULL,
+		Telefone VARCHAR(50) NOT NULL,
+		Parentesco VARCHAR(50) NOT NULL
+	)
+END;
